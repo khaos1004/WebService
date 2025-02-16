656f405f40158cfe3914b3eac9dc6a099ac5e281
Bradon je <khaos1004@naver.com>
2025-02-17 00:24:45 +0900

    Initial commit

---
+++ WebService/src/main/java/com/WebProject/WebService/cofig/QueryDSLConfig.java
@@ -0,0 +1,24 @@
+package com.WebProject.WebService.cofig;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import jakarta.persistence.PersistenceContext;
+
+
+
+
+@RequiredArgsConstructor
+@Configuration
+
+public class QueryDSLConfig {
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Bean
+    public JPAQueryFactory queryFactory() {
+        return new JPAQueryFactory(entityManager);
+    }
+}
