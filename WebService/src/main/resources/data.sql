77645eba73322c01bf049369a41e85fd4c675ac2
Bradon je <khaos1004@naver.com>
2025-02-19 00:01:07 +0900

    done setting

--- WebService/src/main/resources/data.sql
+++ WebService/src/main/resources/data.sql
@@ -1,4 +1,4 @@
 INSERT INTO "user" (username, email, password) VALUES
-                                                   ('john_doe', 'john@example.com', 'password123'),
-                                                   ('jane_doe', 'jane@example.com', 'securepass'),
-                                                   ('mike_smith', 'mike@example.com', 'pass456');
+('john_doe', 'john@example.com', 'password123'),
+('jane_doe', 'jane@example.com', 'securepass'),
+('mike_smith', 'mike@example.com', 'pass456');
